c73a7266b43f0a77b04b6800a5039311bb738271
Hugh <ygz14835187@163.com>
2024-03-24 20:46:35 +0800

    build minify

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ await Bun.build({
   entrypoints: ["./src/index.ts", './src/react.ts'],
   outdir: "./dist",
   target: "browser",
-  // minify: true,
+  minify: true,
   external: ["react", "react-dom", 'zustand'],
   plugins: [dts()],
 });
